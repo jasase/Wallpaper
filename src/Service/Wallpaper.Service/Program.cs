31a3f1351070ee4ee38ffa5a60553619e438871f
Sternheim, Sascha <sascha.sternheim@conti.de>
2019-11-22 12:56:23 +0100

    Create project structure

---
+++ src/Service/Wallpaper.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wallpaper.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
